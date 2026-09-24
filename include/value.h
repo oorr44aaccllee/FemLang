178ce662cd8555ab9df56a37f2b7ce08ad47870a
oorr44aaccllee <matteosteam007@gmail.com>
2026-09-24 11:59:45 -0300

    Fix Value ownership/copy semantics and deep-copy strings in environment lookups and assignments.

--- include/value.h
+++ include/value.h
@@ -27,6 +27,7 @@ Value value_bool(bool value);
 Value value_int(int64_t value);
 Value value_float(double value);
 Value value_string_copy(const char *source);
+Value value_clone(const Value *value);
 void value_free(Value *value);
 void print_value(const Value *value);
 bool value_truthy(const Value *value);
